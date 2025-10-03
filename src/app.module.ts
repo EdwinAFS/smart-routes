609c90ea0d0d3116b74638622369422f92da4390
Edwin Fajardo <alexander_suarez1997@hotmail.com>
2025-10-03 15:11:09 -0500

    Remove smart-routes.db and update database path in TypeORM configuration to '/tmp/smart-routes.db'

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { User } from './user.entity';
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
-      database: 'smart-routes.db',
+      database: '/tmp/smart-routes.db',
       entities: [User],
       synchronize: true,
     }),
