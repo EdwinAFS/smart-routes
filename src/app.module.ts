67e5d62ed6016fb3142770688bcb2949291ba454
Edwin Fajardo <alexander_suarez1997@hotmail.com>
2025-10-20 09:49:47 -0500

    Refactor database path in app.module.ts for improved portability
    - Changed the database path in `app.module.ts` to a relative path, enhancing the application's portability across different environments.
    - Removed the temporary directory creation logic as it is no longer necessary with the updated path.

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,15 +9,12 @@ import { DeviceToken } from './modules/notifications/entities/device-token.entit
 import { DirectionModule } from './modules/directions/direction.module';
 import { OwnTracksModule } from './modules/owntracks/owntracks.module';
 import { OwnTracksPayload } from './modules/owntracks/entities/owntracks-payload.entity';
-import { mkdirSync } from 'fs';
-
-mkdirSync('./tmp', { recursive: true });
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
-      database: './tmp/smart-routes.db',
+      database: 'tmp/smart-routes.db',
       entities: [User, DeviceToken, OwnTracksPayload],
       synchronize: true,
     }),
